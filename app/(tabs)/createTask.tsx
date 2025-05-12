fe8dcef1bb482e800e6186d68f81029d2078d5be
Brenden Bilson <brendenbilson5@gmail.com>
2025-05-11 21:27:04 -0700

    fix type ambiguity and unused import

--- app/(tabs)/createTask.tsx
+++ app/(tabs)/createTask.tsx
@@ -10,7 +10,7 @@ import * as Location from 'expo-location';
 import {View, Text} from '../../components/Themed'
 import { GooglePlacesAutocomplete } from 'react-native-google-places-autocomplete';
 import 'react-native-get-random-values';
-import CreateEventModal from '@/components/CreateEventModal';
+//import CreateEventModal from '@/components/CreateEventModal';
 
 interface SelectedLocation {
     formatted_address: string;
@@ -73,7 +73,7 @@ export default function CreateTask() {
     };
 
     //formatting the date
-    const formatDate = (date) => {
+    const formatDate = (date: Date) => {
         const year = date.getFullYear();
         const month = String(date.getMonth() + 1).padStart(2, '0');
         const day = String(date.getDate()).padStart(2, '0');
